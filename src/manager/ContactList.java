73e7b454f90a05495cb8a8dd7275a5e381ee9fcf
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2015-05-03 22:06:17 +0200

    Integrated Database.

--- src/manager/ContactList.java
+++ src/manager/ContactList.java
@@ -3,31 +3,31 @@ package manager;
 import java.util.ArrayList;
 import java.util.List;
 
+import exceptions.DBException;
 import persons.Contact;
 import main.Core;
 
 public class ContactList {
   private List<Contact> contacts = new ArrayList<Contact>();
   
-  public ContactList() {
+  public ContactList() throws DBException {
     loadFromDatabase();
   }
   
-  public void loadFromDatabase() {
-    
+  public void loadFromDatabase() throws DBException {
+    contacts = Core.getInstance().getDatabase().getContacts();
   }
   
-  public Contact getByUUID(String uuid) {
+  public void addContact(Contact c) throws DBException {
+    contacts.add(c);
+    Core.getInstance().getDatabase().addContact(c);
+  }
+  
+  public Contact getContact(String uuid) throws DBException {
     for (Contact c : contacts)
       if (c.getUUID().equals(uuid))
         return c;
-    Core.getInstance().getUserInterface().printError("Could not find Contact with UUID '" + uuid + "'.");
-    return null;
-  }
-  
-  public void addContact(Contact c) {
-    contacts.add(c);
-    // TODO Add to database.
+    return Core.getInstance().getDatabase().getContact(uuid);
   }
 
 }
