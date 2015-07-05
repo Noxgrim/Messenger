76fc30e42877bd9b7c5fa5c2d55a00f70a13a59e
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2015-07-05 17:20:42 +0200

    Added constructor without database intigration.

--- src/persons/User.java
+++ src/persons/User.java
@@ -3,32 +3,41 @@ package persons;
 public class User {
   private String publicKey;
   private String privateKey;
-  
+
   private String nickname;
   private String uuid;
-  
+
   public User() {
     load();
   }
-  
+
+  public User(String nickname, String uuid, String publicKey, String privateKey) {
+    this.nickname = nickname;
+    this.uuid = uuid;
+    this.privateKey = privateKey;
+    this.publicKey = publicKey;
+  }
+
   private void load() {
     if (nickname.lastIndexOf(0) == 1) {
-      //TODO Load user's data from Database
+      // TODO Load user's data from Database
     }
   }
-  
-  
-  
-  
+
+
+
   public String getUUID() {
     return uuid;
   }
+
   public String getNickname() {
     return nickname;
   }
+
   public String getPrivateKey() {
     return privateKey;
   }
+
   public String getPublicKey() {
     return publicKey;
   }
