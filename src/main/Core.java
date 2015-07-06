9dbcee2f667116e2ab9417782e4a304e691906ec
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2015-07-06 18:25:47 +0200

    Removed test Code

--- src/main/Core.java
+++ src/main/Core.java
@@ -2,7 +2,16 @@ package main;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Stack;
 import java.util.UUID;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 import manager.ConversationManager;
 import manager.MessageManager;
@@ -12,6 +21,7 @@ import persons.User;
 import userInterface.GraphicalUserIterface;
 import userInterface.TerminalUserInterface;
 import userInterface.UserInterface;
+import utils.Formats;
 import utils.HybridCoder;
 import connection.Client;
 import connection.Server;
@@ -25,266 +35,232 @@ import exchange.InternalMessage;
  * Contains the {@code main} method and all important objects of the program.
  */
 public class Core {
-	/** An instance of this object. */
-	public static Core instance;
+  /** An instance of this object. */
+  public static Core instance;
 
-	/** The settings of the program. */
-	private Settings settings;
+  /** The settings of the program. */
+  private Settings settings;
 
-	/** The user interface of the program. */
-	private UserInterface ui;
+  /** The user interface of the program. */
+  private UserInterface ui;
 
-	/** The {@code MessageManager} of the program. */
-	private MessageManager messageManager;
-	/** The <code>ConversationManager</code> of the program. */
-	private ConversationManager cm;
+  /** The {@code MessageManager} of the program. */
+  private MessageManager messageManager;
+  /** The <code>ConversationManager</code> of the program. */
+  private ConversationManager cm;
 
-	/** The internal client. */
-	private Client client;
-	/** The internal server. */
-	private Server server;
-	/** The the data of the program's user. */
-	private User user;
-	/** The database of the program. */
-	private Database db;
+  /** The internal client. */
+  private Client client;
+  /** The internal server. */
+  private Server server;
+  /** The the data of the program's user. */
+  private User user;
+  /** The database of the program. */
+  private Database db;
 
-	/**
-	 * Constructs a new {@code Core} object.
-	 * 
-	 * @param arguments
-	 *            The command line arguments.
-	 */
-	public Core(String[] arguments) {
-		try {
-			init(arguments);
-		} catch (Throwable t) {
-			printError("A fatal error occurred while initialising the program: ", t, true);
-		}
-
-	}
-	
-	/**
-	 * Initialises the program.
-	 * @param args the command line arguments that have been passed to the program.
-	 * @throws Throwable if an exception or error occurred while initialising.
-	 */
-	private void init(String[] args) throws Throwable {
-		
-		
-		settings = new Settings(this);
-		
-		
-		
-		if (25 == 20+5)
-		return;
-
-		if (db == null)
-			db = new Database(settings.getDbLocation());
-
-		ui = (this.settings.getGuiMode()) ? new GraphicalUserIterface(this)
-				: new TerminalUserInterface(this);
-
-		messageManager = new MessageManager();
-
-		client = new Client();
-
-		Thread serverThread = null;
-		try {
-			Server testServer = null;
-			try {
-				server = new Server(this);
-			} catch (Exception e) {
-				ui.printError("Internal Server couldn't be started.", null,
-						true);
-			}
-			serverThread = new Thread(testServer);
-			serverThread.start();
-		} catch (Throwable t) {
-			ui.printError(null, t, true);
-		}
-		
-		
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				shutdown(0);
-			}
-		});
-		
-		
-		if (ui instanceof TerminalUserInterface
-				&& System.getProperty("os.name").toLowerCase()
-						.contains("windows"))
-			settings.setColorShown(false);
-	}
-
-	/**
-	 * Gets the {@link Settings} of the program.
-	 */
-	public Settings getSettings() {
-		return settings;
-	}
-
-	/**
-	 * Gets the {@link UserInterface} of the program.
-	 */
-	public UserInterface getUserInterface() {
-		return ui;
-	}
-
-	/**
-	 * Gets the {@link MessageManager} of the program.
-	 */
-	public MessageManager getMessageManager() {
-		return messageManager;
-	}
-
-	/**
-	 * Gets the {@link ConversationManager} of the program.
-	 */
-	public ConversationManager getConversationManager() {
-		return cm;
-	}
-
-	/**
-	 * Gets the internal {@code Client}.
-	 */
-	public Client getClient() {
-		return client;
-	}
-
-	/**
-	 * Gets the internal {@code Server}.
-	 */
-	public Server getServer() {
-		return server;
-	}
-
-	/**
-	 * Get the data of the program's user.
-	 */
-	public User getUser() {
-		return user;
-	}
-
-	/**
-	 * Get the database.
-	 */
-	public Database getDatabase() {
-		return db;
-	}
-
-	/**
-	 * Prints a message and a StackTrace the screen, optional labelled as fatal
-	 * error.<br>
-	 * If no {@code UserInterface} is available the {@code System.err}-
-	 * {@code PrintStream} will be used.<br>
-	 * <b>Note:</b> The StackTrace is also printed if the {@code printExceptios}
-	 * value of the {@code Settings} object is set to {@code false}.
-	 * <b>Also:</b> If the {@code UserInterface} is not {@code null} it should
-	 * be used instead.
-	 * 
-	 * @param shortMessage
-	 *            The message that will be printed. Can be {@code null}.
-	 * @param t
-	 *            Throwable that's StackTrace will be printed. Can be
-	 *            {@code null}.
-	 * @param fatal
-	 *            whether the Exception is a fatal error or not.
-	 */
-	public void printError(String shortMessage, Throwable t, boolean fatal) {
-		if (ui != null)
-			ui.printError(shortMessage, t, fatal);
-		else {
-			if (!fatal) {
-				if (shortMessage != null)
-					System.err.println(shortMessage);
-
-				if (t != null)
-					t.printStackTrace(System.err);
-			} else {
-				System.err.println("A fatal error occurred:");
-				if (shortMessage != null)
-					System.err.println(shortMessage);
-
-				if (t != null)
-					t.printStackTrace(System.err);
-
-				shutdown(42);
-
-			}
-		}
-	}
-
-	/**
-	 * Closes all resources that have to be closed and shuts down the program.<br>
-	 * This method should be used instead of <code>System.exit()</code>.
-	 * 
-	 * @param status
-	 *            the exit status.
-	 */
-	public void shutdown(int status) {
-		try {
-		  if (db != null)
-			db.close();
-		} catch (DBException e1) {
-			printError("Couldn't close Database.", e1, false);
-		}
-
-		try {
-		  if (server != null)
-			server.close();
-		} catch (IOException e) {
-			Core.getInstance().getUserInterface()
-					.printError("[Server] Couldn't stop internal server!", e);
-		}
-
-		settings.save();
-
-		System.exit(status);
+  /**
+   * Constructs a new {@code Core} object.
+   * 
+   * @param arguments The command line arguments.
+   */
+  public Core(String[] arguments) {
+    try {
+      init(arguments);
+    } catch (Throwable t) {
+      printError("A fatal error occurred while initialising the program: ", t, true);
+    }
 
   }
 
-	/**
-	 * The {@code main} method.<br>
-	 * Creates a new {@code Core} object.
-	 * 
-	 * @param args
-	 *            The arguments of the program.
-	 */
-	public static void main(String[] args) {
-		instance = new Core(args);
-		
-		
-		
-		try {
-		  String[] keyPiar = HybridCoder.generateKeyPair();
-	        
-		  Core.instance.user = new User(Core.instance.getSettings().getOwnNick(), UUID.randomUUID().toString(), keyPiar[1], keyPiar[0]);
-	        
-	        Contact c = new Contact("Peter", UUID.randomUUID().toString(), HybridCoder.generateKeyPair()[1], new InetSocketAddress(55648));
-	        
-	        
-	        InternalMessage im = new InternalMessage("Peter is an awesome guy!", UUID.randomUUID().toString(), c.getUUID());
-	        
-	        System.out.println(im.getFormatted() + "\n\n");
-	        
-	        EncryptedMessage em = im.toEncryptedMessge(c);
-	        
-	        System.out.println(em.getFormatted() + "\n\n");
-	        
-	        im = em.toInternalMessage();
-	        
-	        System.out.println(im.getFormatted() + "\n\n");
-		} catch (Exception e) {
-		  Core.instance.printError("An error occurred.", e, true);
-		}
-	}
+  /**
+   * Initialises the program.
+   * 
+   * @param args the command line arguments that have been passed to the program.
+   * @throws Throwable if an exception or error occurred while initialising.
+   */
+  private void init(String[] args) throws Throwable {
 
-	/**
-	 * Gets an instance of this object.
-	 */
-	public static Core getInstance() {
-		return instance;
-	}
+
+    settings = new Settings(this);
+
+
+
+    if (25 == 20 + 5)
+      return;
+
+    if (db == null)
+      db = new Database(settings.getDbLocation());
+
+    ui =
+        (this.settings.getGuiMode()) ? new GraphicalUserIterface(this) : new TerminalUserInterface(
+            this);
+
+    messageManager = new MessageManager();
+
+    client = new Client();
+
+    Thread serverThread = null;
+    try {
+      Server testServer = null;
+      try {
+        server = new Server(this);
+      } catch (Exception e) {
+        ui.printError("Internal Server couldn't be started.", null, true);
+      }
+      serverThread = new Thread(testServer);
+      serverThread.start();
+    } catch (Throwable t) {
+      ui.printError(null, t, true);
+    }
+
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        shutdown(0);
+      }
+    });
+
+
+    if (ui instanceof TerminalUserInterface
+        && System.getProperty("os.name").toLowerCase().contains("windows"))
+      settings.setColorShown(false);
+  }
+
+  /**
+   * Gets the {@link Settings} of the program.
+   */
+  public Settings getSettings() {
+    return settings;
+  }
+
+  /**
+   * Gets the {@link UserInterface} of the program.
+   */
+  public UserInterface getUserInterface() {
+    return ui;
+  }
+
+  /**
+   * Gets the {@link MessageManager} of the program.
+   */
+  public MessageManager getMessageManager() {
+    return messageManager;
+  }
+
+  /**
+   * Gets the {@link ConversationManager} of the program.
+   */
+  public ConversationManager getConversationManager() {
+    return cm;
+  }
+
+  /**
+   * Gets the internal {@code Client}.
+   */
+  public Client getClient() {
+    return client;
+  }
+
+  /**
+   * Gets the internal {@code Server}.
+   */
+  public Server getServer() {
+    return server;
+  }
+
+  /**
+   * Get the data of the program's user.
+   */
+  public User getUser() {
+    return user;
+  }
+
+  /**
+   * Get the database.
+   */
+  public Database getDatabase() {
+    return db;
+  }
+
+  /**
+   * Prints a message and a StackTrace the screen, optional labelled as fatal error.<br>
+   * If no {@code UserInterface} is available the {@code System.err}- {@code PrintStream} will be
+   * used.<br>
+   * <b>Note:</b> The StackTrace is also printed if the {@code printExceptios} value of the
+   * {@code Settings} object is set to {@code false}. <b>Also:</b> If the {@code UserInterface} is
+   * not {@code null} it should be used instead.
+   * 
+   * @param shortMessage The message that will be printed. Can be {@code null}.
+   * @param t Throwable that's StackTrace will be printed. Can be {@code null}.
+   * @param fatal whether the Exception is a fatal error or not.
+   */
+  public void printError(String shortMessage, Throwable t, boolean fatal) {
+    if (ui != null)
+      ui.printError(shortMessage, t, fatal);
+    else {
+      if (!fatal) {
+        if (shortMessage != null)
+          System.err.println(shortMessage);
+
+        if (t != null)
+          t.printStackTrace(System.err);
+      } else {
+        System.err.println("A fatal error occurred:");
+        if (shortMessage != null)
+          System.err.println(shortMessage);
+
+        if (t != null)
+          t.printStackTrace(System.err);
+
+        shutdown(42);
+
+      }
+    }
+  }
+
+  /**
+   * Closes all resources that have to be closed and shuts down the program.<br>
+   * This method should be used instead of <code>System.exit()</code>.
+   * 
+   * @param status the exit status.
+   */
+  public void shutdown(int status) {
+    try {
+      if (db != null)
+        db.close();
+    } catch (DBException e1) {
+      printError("Couldn't close Database.", e1, false);
+    }
+
+    try {
+      if (server != null)
+        server.close();
+    } catch (IOException e) {
+      Core.getInstance().getUserInterface()
+          .printError("[Server] Couldn't stop internal server!", e);
+    }
+
+    settings.save();
+
+    System.exit(status);
+
+  }
+
+  /**
+   * The {@code main} method.<br>
+   * Creates a new {@code Core} object.
+   * 
+   * @param args The arguments of the program.
+   */
+  public static void main(String[] args) {
+    instance = new Core(args);
+  }
+
+  /**
+   * Gets an instance of this object.
+   */
+  public static Core getInstance() {
+    return instance;
+  }
 }
